621c920ad90b4a6e07cf08fdc0e43fb0559621d4
İsmail Enes Ayhan <36127004+ismailenesayhan@users.noreply.github.com>
2022-11-24 12:10:52 +0300

    Create order-post-by-menu-order-on-category-page.php
    Order posts by "menu_order" on category post listing page

---
+++ order-post-by-menu-order-on-category-page.php
@@ -0,0 +1,8 @@
+function sort_post_in_category_page($query) {
+    if (!is_category() && is_tax('product_category')) {
+        $query -> set('order', 'ASC');
+        $query -> set('orderby', 'menu_order');
+    }
+}
+
+add_action('pre_get_posts', 'sort_post_in_category_page');
